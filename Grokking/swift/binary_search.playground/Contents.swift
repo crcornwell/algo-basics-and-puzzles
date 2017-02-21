09709e4c329ede1758d5098fdcb0af5c967f2b02
Riley Gibbs <riley@citizennet.com>
2017-02-21 13:38:30 -0800

    Recursion sucks. While loop is easier.

--- Grokking/swift/binary_search.playground/Contents.swift
+++ Grokking/swift/binary_search.playground/Contents.swift
@@ -3,24 +3,22 @@
 var myList = [1,2,3,5,8,13,21,34,55,89];
 
 func binarySearch(list: Array<Int>, item: Int) -> Int? {
-    let middleIndex = list.count / 2;
-    if ((list.count == 1) && (list[0] != item)) {
-        return nil;
-    }
-    if (list[middleIndex] == item) {
-        return middleIndex;
-    } else if (list[middleIndex] > item) {
-        let firstHalf = Array(list.prefix(middleIndex));
-        return binarySearch(list: firstHalf, item: item);
-    } else {
-        let secondHalf = Array(list.suffix(list.count - middleIndex));
-        let recurse = binarySearch(list: secondHalf, item: item);
-        if (recurse != nil) {
-            return recurse! + middleIndex;
+    var low = 0;
+    var high = list.count - 1;
+    var mid: Int;
+    var guess: Int;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        guess = list[mid];
+        if (guess == item) {
+            return mid;
+        } else if (guess > item) {
+            high = mid - 1;
         } else {
-            return nil;
+            low = mid + 1;
         }
     }
+    return nil;
 }
 
 // Test for each item in the list.
